d4dc69097ce9f2649229d44d0d21b366fec283b0
artprojectgroup <artprojectgroup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-12 10:24:03 +0000

    La primera versión del plugin
    git-svn-id: https://plugins.svn.wordpress.org/crosspress-2/trunk@739689 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- crosspress.php
+++ crosspress.php
@@ -1,29 +1,16 @@
 <?php
 /*
 Plugin Name: CrossPress 2
-Description: Automatically, cross-posting to assiciated site/blog to enabling the post-via-email option with PIN code e.g. livejournal.com, tumblr.com, blogspot.com, wordpress.com and much more. Created from <a href="http://wordpress.org/plugins/crosspress/">Atthakorn Chanthong</a> <strong>CrossPress</strong>.
+Description: Gracias a CrossPress 2 podremos publicar automáticamente las entradas que publiquemos en nuestro sitio web bajo WordPress en otros servicios. Creado a partir del plugin de <a href="http://www.atthakorn.com/project/crosspress/">Atthakorn Chanthong</a> <a href="http://wordpress.org/plugins/crosspress/"><strong>CrossPress</strong></a>.
 Version: 1.0
+Plugin URI: http://wordpress.org/plugins/crosspress-2/
 Author: Art Project Group
 Author URI: http://www.artprojectgroup.es/
 */
 
-/*  Copyright 2013 Art Project Group  (info@artprojectgroup.es)
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
+/**
+ * Initializes the plugin
+ */
 class CrossPress
 {
 	var $saved = false;
@@ -61,8 +48,6 @@ class CrossPress
 		print'<form style="padding-left:25px;" method="post" action="">';
 		
 		print '<div>';
-		//print 'Secret Pin: <input name="pin" type=\"text\" size="50" value="'.stripcslashes(get_option('crosspress_pin')).'"><br />';
-		//print 'Signature : <input name="signature" type=\"text\" size="50" value="'.stripcslashes(get_option('crosspress_signature')).'"><br />';
 		print '<b>PIN secreto:</b><br />';
 		print 'Código PIN (correo electrónico) que le permite publicar automáticamente en su sitio/blog por correo electrónico.<br />';
 		print 'Cada PIN debe intoducirse en una nueva línea.<br />';
@@ -71,7 +56,6 @@ class CrossPress
 		print '<b>Firma :</b><br />';
 		print 'Puede añadir una firma si lo desea. Las URLs introducidas serán convertidas en enlaces automáticamente.<br />';
 		print '<textarea name="signature" cols="50" rows="5">'.stripcslashes(get_option('crosspress_signature')).'</textarea><br /><br />';
-		//print '<input name="summarytext" type="checkbox" value="'.(strcmp(get_option('crosspress_summary'),"yes")?  "yes":  "no").'" '.(strcmp(get_option('crosspress_summary'),"yes")?  "checked":  "").'> Show summary text.';
 		print '<input name="summarytext" type="checkbox" value="1" '.(get_option('crosspress_summary') == "1" ? "checked":  "").'> Mostrar resumen.';
 		print '</div>';
 		print '<div><input type="submit" value="Guardar &raquo;"></div>';
@@ -79,35 +63,37 @@ class CrossPress
 		print'</form></div>';
 		print '<br />';
 		print '<br />';
-		//print $this->getValidAddress(get_option("crosspress_pin"));
 	}
 
 	function add_action() {
 		add_action('publish_post', array(&$this, 'post_2_blog'));
-		//add_action('save_post', array(&$this, 'post_2_blog'));
 	}
 
  	function post_2_blog($postid) {
 		$post = get_post($postid);
 		setup_postdata($post);
 		$excerpt = get_the_excerpt();
+		$content = get_the_content();
 		
 		//If post time is not equally to modified time, skip sending mail
 		if ($post->post_date == $post->post_modified)
 		{
 			//Partes del correo	
 			$to = htmlspecialchars($this->getValidAddress(get_option("crosspress_pin")));
-			//$to .= ", info@artprojectgroup.com";
 			$subject = "=?UTF-8?B?".base64_encode($post->post_title)."?=";
 			$headers = "Content-Type: text/html;charset=utf8";
 			
 			if (get_option('crosspress_summary') == "1")
 			{
 				if (!empty($post->post_excerpt)) $msg = the_excerpt();
-				else $msg = $excerpt;//$this->wp_new_excerpt(the_content());
+				else $msg = $excerpt;
 				$msg .= '<br /><br />Sigue leyendo en <a href="'.get_permalink($postid).'" title="'.$post->post_title.' en Art Project Group">'.$post->post_title.'</a>.';   
 			}
-			else $msg = the_content();
+			else 
+			{
+				if (!empty($post->post_content)) $msg = the_content();
+				else $msg = $content;
+			}
 			
 			$msg .= ' '.'<br />';
 			$msg .= ' '.'<br />';
@@ -129,7 +115,6 @@ class CrossPress
 				preg_match_all('/[\w\.=-]+@[a-zA-Z0-9_\-\.]+wordpress.com/', $to, $wordpress);
 				
 				if (isset($wordpress[0][0])) $to = htmlspecialchars($this->getValidAddress(get_option("crosspress_pin"), $wordpress[0][0])); //Quitamos del primer correo la cuenta de WordPress.com
-				//$wordpress[0][0] .= ", info@artprojectgroup.com";
 			}
 			
 			//sending mail
